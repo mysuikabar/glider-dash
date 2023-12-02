55d2fe376d510e2d4333a45c7ef420af58d8bfa2
suikabar <mizuho.suikabar@gmail.com>
2023-12-02 09:33:41 +0000

    update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,21 @@
 import dash
+import dash_bootstrap_components as dbc
 from dash import html
 
 dash.register_page(__name__, path="/")
 
-layout = html.Div([html.H4("Welcome to Glider Dashboard!")])
+sidebar = html.Div([])
+content = html.Div([html.H4("Welcome to Glider Dashboard!")])
+
+layout = layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(sidebar, width=2, className="bg-light"),
+                dbc.Col(content, width=10),
+            ],
+            style={"height": "100vh"},
+        ),
+    ],
+    fluid=True,
+)
