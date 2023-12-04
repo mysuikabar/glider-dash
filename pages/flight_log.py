24f5cd89f2b81e42205e05315fbfc74a990583d1
suikabar <mizuho.suikabar@gmail.com>
2023-12-04 15:57:33 +0000

    update flight_log.py
    - add igc file uploader

--- pages/flight_log.py
+++ pages/flight_log.py
@@ -1,19 +1,32 @@
+from pathlib import Path
+from typing import List
+
 import dash
 import dash_bootstrap_components as dbc
+import dash_uploader as du
 import pandas as pd
 import plotly.graph_objects as go
-from dash import Input, Output, callback, dcc, html
+from dash import Input, Output, State, callback, dcc, html
 
 from src import stats
-from src.consts import LOG_DATA_DIR, MAP_CENTER
+from src.consts import MAP_CENTER, TMP_DIR
+from src.preprocessing.circling import compute_heading_transition, detect_circling
+from src.preprocessing.igc import igc2csv
 
-DATA_DIR = LOG_DATA_DIR / "csv"
 dash.register_page(__name__)
+du.configure_upload(dash.get_app(), TMP_DIR)
 
 
-igc_files = [file.stem for file in DATA_DIR.glob("*.csv")]
 sidebar = html.Div(
-    dcc.Dropdown(igc_files, id="igc-files-dropdown", optionHeight=50, multi=True)
+    [
+        du.Upload(
+            text="Upload IGC File",
+            filetypes=["igc"],
+            id="igc-files-uploader",
+        ),
+        dcc.Dropdown([], optionHeight=50, multi=True, id="igc-files-dropdown"),
+        dcc.Store(id="current-tmp-dir"),
+    ]
 )
 
 content = html.Div(
@@ -48,11 +61,35 @@ layout = dbc.Container(
 )
 
 
+@du.callback(output=Output("current-tmp-dir", "data"), id="igc-files-uploader")
+def preprocess_igc(files: List[str]):
+    for file in files:
+        df = igc2csv(file)
+        df["heading"] = compute_heading_transition(df["latitude"], df["longitude"])
+        df["circling"] = detect_circling(df["heading"])
+        df.to_csv(Path(file).with_suffix(".csv"), index=False)
+
+    current_tmp_dir = Path(files[0]).parent
+    return str(current_tmp_dir)
+
+
+@callback(
+    Output(component_id="igc-files-dropdown", component_property="options"),
+    Input(component_id="current-tmp-dir", component_property="data"),
+)
+def update_dropdown(current_tmp_dir: str):
+    if current_tmp_dir is None:
+        return []
+
+    return [file.stem for file in Path(current_tmp_dir).glob("*.csv")]
+
+
 @callback(
     Output(component_id="trajectory", component_property="figure"),
     Input(component_id="igc-files-dropdown", component_property="value"),
+    State(component_id="current-tmp-dir", component_property="data"),
 )
-def update_trajectory(files):
+def update_trajectory(files: List[str], current_tmp_dir: str):
     fig = go.Figure()
     fig.update_layout(
         margin={"l": 0, "t": 0, "b": 0, "r": 0},
@@ -69,7 +106,7 @@ def update_trajectory(files):
         return fig
 
     for file in files:
-        df = pd.read_csv(DATA_DIR / f"{file}.csv")
+        df = pd.read_csv(Path(current_tmp_dir) / f"{file}.csv")
         fig.add_trace(
             go.Scattermapbox(
                 mode="lines",
@@ -87,8 +124,9 @@ def update_trajectory(files):
 @callback(
     Output(component_id="altitude", component_property="figure"),
     Input(component_id="igc-files-dropdown", component_property="value"),
+    State(component_id="current-tmp-dir", component_property="data"),
 )
-def update_altitude(files):
+def update_altitude(files: List[str], current_tmp_dir: str):
     fig = go.Figure()
     fig.update_layout(
         xaxis_title="time",
@@ -101,7 +139,7 @@ def update_altitude(files):
         return fig
 
     for file in files:
-        df = pd.read_csv(DATA_DIR / f"{file}.csv")
+        df = pd.read_csv(Path(current_tmp_dir) / f"{file}.csv")
         fig.add_trace(go.Scatter(x=df["timestamp"], y=df["altitude(press)"], name=file))
 
     return fig
@@ -110,8 +148,9 @@ def update_altitude(files):
 @callback(
     Output(component_id="climb-rate", component_property="figure"),
     Input(component_id="igc-files-dropdown", component_property="value"),
+    State(component_id="current-tmp-dir", component_property="data"),
 )
-def update_climb_rate(files):
+def update_climb_rate(files: List[str], current_tmp_dir: str):
     fig = go.Figure()
     fig.update_layout(yaxis_title="climb rate (m/s)", showlegend=False)
 
@@ -119,7 +158,7 @@ def update_climb_rate(files):
         return fig
 
     for file in files:
-        df = pd.read_csv(DATA_DIR / f"{file}.csv")
+        df = pd.read_csv(Path(current_tmp_dir) / f"{file}.csv")
         df["timestamp"] = pd.to_datetime(df["timestamp"])
         climb_rate = stats.average_climb_rate(df)
         fig.add_trace(
--- src/consts.py
+++ src/consts.py
@@ -4,4 +4,6 @@ ROOT = Path(__file__).parents[1]
 LOG_DATA_DIR = ROOT / "data" / "log"
 AMEDAS_DATA_DIR = ROOT / "data" / "amedas"
 
+TMP_DIR = Path("/tmp/uploads")
+
 MAP_CENTER = {"lon": 139.41889, "lat": 36.21139}  # 妻沼の座標
