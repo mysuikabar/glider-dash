04ef3b6952d50cc4dbd781dcc2f9d14e767d9b5e
suikabar <mizuho.suikabar@gmail.com>
2023-12-08 15:49:16 +0000

    add fetch_data.py
    - fetch files from S3

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+import os
+
 import dash
 import dash_bootstrap_components as dbc
 from dash import Dash, html
 from dotenv import load_dotenv
 
+from src.consts import ROOT
+from src.fetch_data import fetch_files_from_s3
+
 load_dotenv()
+
+bucket_name = os.getenv("BUCKET_NAME")
+fetch_files_from_s3(bucket_name, "log/agg/", ROOT / "data/log/agg/")
+fetch_files_from_s3(bucket_name, "amedas/processed/", ROOT / "data/amedas/processed/")
+
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY], use_pages=True)
 
---
+++ src/fetch_data.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import boto3
+
+
+def fetch_files_from_s3(bucket_name: str, s3_folder: str, local_dir: Path):
+    """
+    S3上の指定したフォルダのcsvファイルをローカルにダウンロードする
+    """
+    if not local_dir.exists():
+        local_dir.mkdir(parents=True)
+
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(bucket_name)
+
+    for obj in bucket.objects.filter(Prefix=s3_folder):
+        if obj.key.endswith(".csv"):
+            file_path = local_dir / obj.key.split("/")[-1]
+
+            if file_path.exists():
+                continue
+            else:
+                bucket.download_file(obj.key, file_path)
